e3028eb98e3fab8343680da27f523ee96af35a24
josh16 <josh.randall16@gmail.com>
2017-07-27 18:59:14 -0400

    Uploading to Github
    -Importing Project and assets to github

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour {
+
+
+    public Vector3 rayOrigin;
+    public float rayDistance;
+
+    LayerMask door;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hit;
+        Ray lightRay = new Ray(rayOrigin, Vector3.forward);
+
+        Physics.Raycast(lightRay, out hit, door);
+
+    }
+}
