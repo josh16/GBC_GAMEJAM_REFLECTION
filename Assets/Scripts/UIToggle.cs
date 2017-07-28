4c9228e01428c966902a1d2cb65145fd2dba0b0b
Joshua Randall <josh.randall16@gmail.com>
2017-07-27 23:07:36 -0400

    Added Scripts
    -Added playerController script
     *Player controls for picking up game objects
    -Added UI toggle script
    *UI menu pops up for the player to interact with

---
+++ Assets/Scripts/UIToggle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIToggle : MonoBehaviour {
+
+
+
+
+	private bool pickCandle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Once player enters trigger, UI text pops up
+		if (other.gameObject.tag == "Player") 
+		{
+			
+			//Display Pop up message that says "Press 'E' to pick up"
+			pickCandle = true;
+		}
+
+
+
+
+
+	}
+
+
+	void OnTriggerExit(Collider other)
+	{
+
+		pickCandle = false;
+	}
+
+
+	//GUI FUNCTION
+	void OnGUI()
+	{
+		if (pickCandle == true) 
+		{
+			if (GUI.Button ( new Rect (350, 100, 500, 40), "Press 'E' to pick up Candle")) 
+			{
+				Debug.Log("Message popped up!");
+				pickCandle = false;
+			}
+
+		}
+
+
+	}
+
+
+}
---
+++ Assets/Scripts/playerControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour {
+
+
+	public Transform handTransform;
+	public GameObject candleObject;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		KeyboardControls ();		
+	}
+
+
+
+	void KeyboardControls()
+	{
+		if (Input.GetKey(KeyCode.E)) 
+		{
+			//Pick up the gameobject
+			candleObject.SetActive(false);
+			//GameObject get's attached to player's transform
+			//The GameObject becomes 'Visible'
+		}
+
+	}
+
+
+
+}
